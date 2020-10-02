1f26ae5ceddf73b3912309870cb1459b50f8cbfe
sumit <spsingh02497@gmail.com>
2020-10-02 12:02:09 +0530

    messaging code pushed to github

---
+++ src/main/java/com/gain/java/knowledge/messaging/controller/MessageController.java
@@ -0,0 +1,13 @@
+package com.gain.java.knowledge.messaging.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Aws code pipeline deployement";
+	}
+}
